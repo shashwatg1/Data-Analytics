f6632529db76a09e61db671b78446bebd38b7443
shashwatg1 <shashwatg1@gmail.com>
2016-12-16 16:33:46 +0530

    Changes

--- Analytics Edge Course/4 Trees/Assignment 3/code.R
+++ Analytics Edge Course/4 Trees/Assignment 3/code.R
@@ -1,1 +1,70 @@
 # Read Data
+dataset = read.csv("census.csv")
+library(caTools)
+set.seed(2000)
+spl = sample.split(dataset$over50k, SplitRatio = 0.6)
+train = subset(dataset, spl == TRUE)
+test = subset(dataset, spl == FALSE)
+
+# Logistic Reg model
+lgmodel = glm(over50k ~ ., data = train, family = "binomial")
+summary(lgmodel)
+
+predictions = predict(lgmodel, newdata = test, type = "response")
+table(test$over50k,predictions >= 0.5) # Accuracy = 0.855
+
+#Baseline Model
+table(train$over50k) # predict all <= 50k
+table(test$over50k) # accuracy = 0.759
+
+library(ROCR)
+ROCRpred = prediction(predictions,test$over50k)
+as.numeric(performance(ROCRpred, "auc")@y.values) #AUC value
+ROCRperf = performance(ROCRpred, "tpr", "fpr")
+plot(ROCRperf)
+
+#CART Model
+library(rpart)
+library(rpart.plot)
+trmodel = rpart(over50k ~ ., data = train, method = "class")
+prp(trmodel)
+predictions = predict(trmodel, newdata = test)
+table(test$over50k,predictions[,2]>=0.5) # accuracy = .847
+
+ROCRpred = prediction(predictions[,2],test$over50k)
+as.numeric(performance(ROCRpred, "auc")@y.values) #AUC value
+ROCRperf = performance(ROCRpred, "tpr", "fpr")
+plot(ROCRperf)
+
+# RF Model
+library(randomForest)
+# Down sampling the training dataset
+set.seed(1)
+trainSmall = train[sample(nrow(train), 2000), ]
+set.seed(1)
+rfmodel = randomForest(over50k ~., data = trainSmall)
+predictions = predict(rfmodel, newdata = test)
+table(test$over50k, predictions) # accuracy = 0.8351184
+
+# This code produces a chart that for each variable measures the number of times that variable was selected for splitting
+vu = varUsed(rfmodel, count=TRUE)
+vusorted = sort(vu, decreasing = FALSE, index.return = TRUE)
+dotchart(vusorted$x, names(rfmodel$forest$xlevels[vusorted$ix]))
+
+varImpPlot(rfmodel)
+# A different metric we can look at is related to "impurity", which measures how homogenous each bucket or leaf of the tree is.
+# In each tree in the forest, whenever we select a variable and perform a split, the impurity is decreased.
+# Therefore, one way to measure the importance of a variable is to average the reduction in impurity, taken over all the times
+# that variable is selected for splitting in all of the trees in the forest. This plot shows exactly that only.
+
+# Cross Validation to selct CP
+library(caret)
+set.seed(2)
+fitControl = trainControl( method = "cv", number = 10 )
+cartGrid = expand.grid( .cp = seq(0.002,0.1,0.002)) 
+train( over50k ~ . , data = train, method = "rpart", trControl = fitControl, tuneGrid = cartGrid ) # cp = 0.002
+
+model = rpart(over50k~., data=train, method="class", cp=0.002)
+prp(model)
+predictions = predict(model, newdata = test)
+table(test$over50k,predictions[,2]>=0.5) # accuracy = .86
--- Analytics Edge Course/4 Trees/Assignment 4/code.R
+++ Analytics Edge Course/4 Trees/Assignment 4/code.R
@@ -1,1 +1,53 @@
+library(caTools)
+library(ROCR)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+library(caret)
+
 # Read Data
+statedata = read.csv("statedataSimple.csv")
+str(statedata)
+
+# Linear Regression
+lrmodel = lm(Life.Exp ~ ., data = statedata)
+summary(lrmodel)
+sum((lrmodel$residuals)^2) # SSE
+
+lrmodel2 = lm(Life.Exp ~ Population + Murder + Frost + HS.Grad, data = statedata)
+summary(lrmodel2)
+sum((lrmodel2$residuals)^2) # SSE
+
+# CART Model
+cartmodel = rpart(Life.Exp ~ ., data = statedata)
+prp(cartmodel)
+pred = predict(cartmodel)
+sum((pred-statedata$Life.Exp)^2)
+
+cartmodel2 = rpart(Life.Exp ~ ., data = statedata, minbucket =5)
+prp(cartmodel2)
+pred2 = predict(cartmodel2)
+sum((pred2-statedata$Life.Exp)^2)
+
+cartmodel3 = rpart(Life.Exp ~ Area, data = statedata, minbucket =1)
+prp(cartmodel3)
+pred3 = predict(cartmodel3)
+sum((pred3-statedata$Life.Exp)^2)
+
+# Cross Validation
+set.seed(111)
+fitControl = trainControl( method = "cv", number = 10 )
+cartGrid = expand.grid( .cp = seq(0.01,0.5,0.01)) 
+train( Life.Exp ~ . , data = statedata, method = "rpart", trControl = fitControl, tuneGrid = cartGrid ) # cp = 0.11
+model = rpart(Life.Exp~., data=statedata, cp=0.11)
+prp(model)
+pred = predict(model)
+sum((pred-statedata$Life.Exp)^2)
+
+
+set.seed(111)
+train( Life.Exp ~ Area, data = statedata, method = "rpart", trControl = fitControl, tuneGrid = cartGrid ) # cp = 0.06
+model2 = rpart(Life.Exp~Area, data=statedata, cp=0.06)
+prp(model2)
+pred = predict(model2)
+sum((pred-statedata$Life.Exp)^2)
