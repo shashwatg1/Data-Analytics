629c57ad0c5cbd5e7954c94415636187ea0f4a26
shashwatg1 <shashwatg1@gmail.com>
2016-12-20 17:00:09 +0530

    Changes

--- Analytics Edge Course/6 Clustering/3 Big Picture/Unit6_Recitation.R
+++ Analytics Edge Course/6 Clustering/3 Big Picture/Unit6_Recitation.R
@@ -1,44 +1,64 @@
+# Flower is a simple sample example. We are actually interested in the MRI images helathy and tumor.
+# The gray-scale image information is provided in the csv files
+# The images are represented as a matrix of the pixel intensity values between 0 and 1. 8 bit hence 256 levels between 0 to 1.
+# To apply cluster to these images, we can cluster pixels according to their intensity values
+# To do this we can convert the matrix to a long vector and then assign each pixel value to a cluster.
+
+## for hierarchical clustering, we need to find distances (pairwise) between the vector elements
+
 flower = read.csv("flower.csv", header=FALSE)
 str(flower)
 
+# Remember that R treats Rows as Observations and Columsn as variables
+
 # Change the data type to matrix
 flowerMatrix = as.matrix(flower)
-str(flowerMatrix)
+str(flowerMatrix) # Thus 50 X 50 pixel image
 
 # Turn matrix into a vector
 flowerVector = as.vector(flowerMatrix)
-str(flowerVector)
+str(flowerVector) # 2500 values as expected
 
+# We converted to matrix and then to vector, if we turn to vector directly then the following happens
 flowerVector2 = as.vector(flower)
-str(flowerVector2) # hence we cant do directly
+str(flowerVector2) # Same as the data frame itself, hence we cant do directly
 
 # Compute distances
 distance = dist(flowerVector, method = "euclidean")
 
-
 # Hierarchical clustering
-clusterIntensity = hclust(distance, method="ward.D")
+clusterIntensity = hclust(distance, method="ward.D") # ward basically minimises the variance and the distance among clusters
 
 # Plot the dendrogram
 plot(clusterIntensity)
 
-# Select 3 clusters
+# Select 3 clusters 
+# Visualise the cut
 rect.hclust(clusterIntensity, k = 3, border = "red")
+
+# Making the cut
 flowerClusters = cutree(clusterIntensity, k = 3)
-flowerClusters
+flowerClusters # a vector of 2500 variables stating the cluster of each pixel value
 
 # Find mean intensity values
 tapply(flowerVector, flowerClusters, mean)
+# So it looks like the 1st cluster has very low intensity value pixels (black) and 3rd cluster has high intensity value pixels (white)
 
 # Plot the image and the clusters
+# convert the vector to a matrix
 dim(flowerClusters) = c(50,50)
+# To output the image based on the input matrix. Each colour will correspond to a particular cluster
 image(flowerClusters, axes = FALSE)
 
 # Original image
 image(flowerMatrix,axes=FALSE,col=grey(seq(0,1,length=256)))
 
+# So we have done a great job using the hierarchical clustering method
 
-# Let's try this with an MRI image of the brain
+# In this flower example, we understood the clustering algorithm's potential, however in this case the image was very low resolution
+
+# Now lets work on teh high resolution MRI images of the brain
+
 healthy = read.csv("healthy.csv", header=FALSE)
 healthyMatrix = as.matrix(healthy)
 str(healthyMatrix)
